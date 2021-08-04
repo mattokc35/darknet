1ef1d13569276288979c4f1a126e7048a86b946c
Matthew Chen <mattokc35ibm.com@Matthews-MacBook-Pro.local>
2021-08-04 11:39:01 -0500

    fix traintestsplit for current path

--- traintestsplit.py
+++ traintestsplit.py
@@ -2,22 +2,23 @@ import glob, os
 # Current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 cwd = os.getcwd()
-os.chdir('../')
-cwd = os.getcwd()
 
-print(cwd)
-#current_dir = '/Users/mattokc35ibm.com/spacetech-PLASTICNET/7classdarknetyolo/darknet/data/obj'
+os.chdir('data/obj')
+cwd = os.getcwd()
+os.chdir('../')
+dataDir = os.getcwd()
+
 # Percentage of images to be used for the test set
 percentage_test = 20;
 # Create and/or truncate train.txt and test.txt
 
-file_train = open(str(cwd) + '/train.txt', 'w')
-file_test = open(str(cwd) + '/test.txt', 'w')
-"""
+file_train = open(str(dataDir) + '/train.txt', 'w')
+file_test = open(str(dataDir) + '/test.txt', 'w')
+
 # Populate train.txt and test.txt
 counter = 1
 index_test = round(100 / percentage_test)
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+for pathAndFilename in glob.iglob(os.path.join(cwd, "*.jpg")):
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter == index_test:
         counter = 1
@@ -25,4 +26,4 @@ for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
     else:
         file_train.write("data/obj" + "/" + title + '.jpg' + "\n")
         counter = counter + 1
-"""      
+     
